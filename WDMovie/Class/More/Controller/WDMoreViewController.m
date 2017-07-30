bd594eb3a743da5af8cb6d709ba29665302d92b3
Mac <mac@MacdeMBP.lan>
2017-07-30 23:49:30 +0800

    增加功能

--- WDMovie/Class/More/Controller/WDMoreViewController.m
+++ WDMovie/Class/More/Controller/WDMoreViewController.m
@@ -9,11 +9,15 @@
 #import "WDMoreViewController.h"
 #import "WDScannerViewController.h"
 
+static NSInteger const headerHeight = 160;
 static NSString *kMoreTableViewCellID = @"kMoreTableViewCellID";
 
 @interface WDMoreViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @property(nonatomic,strong)UITableView *tableView;
+@property(nonatomic,strong)UIImageView *backgroundView;
+@property(nonatomic,strong)UIView *navigationView;
+@property(nonatomic,assign)CGRect originalFrame;
 
 @end
 
@@ -21,33 +25,71 @@ static NSString *kMoreTableViewCellID = @"kMoreTableViewCellID";
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    [self setNavigationBarStyle:HistoryStyle];
+    [self setNavigationBarStyle:ClearStyle];
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"更多";
     self.view.backgroundColor = WD_COLOR.background;
+    
+    _backgroundView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, headerHeight + 64)];
+    _backgroundView.backgroundColor = [UIColor clearColor];
+    _backgroundView.image = [UIImage imageNamed:@"header_view"];
+    [self.view addSubview:_backgroundView];
+    _originalFrame = _backgroundView.frame;
+    
     [self initTableView];
+    
+    _navigationView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 64)];
+    _navigationView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:_navigationView];
+    
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Scan_icon"]
                                                                               style:UIBarButtonItemStylePlain
                                                                              target:self
                                                                              action:@selector(scanBarcode)];
 }
-- (void)scanBarcode{
-    WDScannerViewController *scannerVC = [[WDScannerViewController alloc]init];
-    scannerVC.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:scannerVC animated:YES];
-}
+
 - (void)initTableView{
-    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 113) style:UITableViewStyleGrouped];
+    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, SCREEN_HEIGHT - 113) style:UITableViewStyleGrouped];
     self.tableView.backgroundColor = [UIColor clearColor];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     [self.view addSubview:self.tableView];
+    
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, headerHeight)];
+    headerView.backgroundColor = [UIColor clearColor];
+    self.tableView.tableHeaderView = headerView;
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat yoffset = scrollView.contentOffset.y;
+    if (yoffset < headerHeight) {
+        CGFloat colorAlpha = yoffset/headerHeight;
+        self.navigationView.backgroundColor = [WD_COLOR.navBarItem colorWithAlphaComponent:colorAlpha];
+    } else {
+        self.navigationView.backgroundColor = WD_COLOR.navBarItem;
+    }
+    if (yoffset > 0) {
+        //复合语句
+        self.backgroundView.frame = ({
+            CGRect frame = self.originalFrame;
+            frame.origin.y = self.originalFrame.origin.y - yoffset;
+            frame;
+        });
+    } else {
+        self.backgroundView.frame = ({
+            CGRect frame = self.originalFrame;
+            frame.size.height = self.originalFrame.size.height - yoffset;
+            frame.size.width = (self.originalFrame.size.height * SCREEN_WIDTH)/(headerHeight + 64);
+            frame.origin.x = self.originalFrame.origin.x - (frame.size.width - self.originalFrame.size.width)/2;
+            frame;
+        });
+    }
+}
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 3;
+    return 5;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if (section == 0) {
@@ -69,7 +111,11 @@ static NSString *kMoreTableViewCellID = @"kMoreTableViewCellID";
     cell.textLabel.text = WDFORMAT(@"标题%ld",indexPath.row + 1);
     return cell;
 }
-
+- (void)scanBarcode{
+    WDScannerViewController *scannerVC = [[WDScannerViewController alloc]init];
+    scannerVC.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:scannerVC animated:YES];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
